a87fd56b3ed57368524f5769424f6d21a3b31029
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2024-05-02 18:51:55 +0200

    all: update version

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
-#define UPX_VERSION_HEX      0x040300 /* 04.03.00 */
-#define UPX_VERSION_STRING   "4.3.0"
-#define UPX_VERSION_STRING4  "4.30"
-#define UPX_VERSION_DATE     "Apr 21st 2024"
-#define UPX_VERSION_DATE_ISO "2024-04-21"
+#define UPX_VERSION_HEX      0x040204 /* 04.02.04 */
+#define UPX_VERSION_STRING   "4.2.4"
+#define UPX_VERSION_STRING4  "4.24"
+#define UPX_VERSION_DATE     "May 2nd 2024"
+#define UPX_VERSION_DATE_ISO "2024-05-02"
 #define UPX_VERSION_YEAR     "2024"
