e1fcfb56e6148bfdf6950056272a228022fa7621
zihelyu <zihelyu@dawnwl.com>
2024-05-29 23:38:18 +0800

    Update conf.h

--- src/conf.h
+++ src/conf.h
@@ -137,7 +137,7 @@ inline constexpr bool upx_is_integral_v = upx_is_integral<T>::value;
 // this limits uncompressed_size to about 682 MiB (715_128_832 bytes)
 typedef size_t upx_rsize_t;
 #define UPX_RSIZE_MAX     UPX_RSIZE_MAX_MEM
-#define UPX_RSIZE_MAX_MEM (1000 * 1024 * 1024)
+#define UPX_RSIZE_MAX_MEM (900 * 1024 * 1024)
 #define UPX_RSIZE_MAX_STR (1024 * 1024)
 
 // integral types
